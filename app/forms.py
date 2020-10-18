1b766331de8f53a08d2c07ade047b64aa0e9d91e
hamano.shogo <hamano.shogo.nalgo.intern@gmail.com>
2020-10-18 14:33:44 +0900

    とりあえずあげます

---
+++ app/forms.py
@@ -0,0 +1,72 @@
+from django import forms
+from django.contrib.auth.forms import (
+    AuthenticationForm, UserCreationForm
+)
+from django.contrib.auth import get_user_model
+from .models import User, Student, Society
+
+User = User
+Student = Student
+Society = Society
+
+class LoginForm(AuthenticationForm):
+    """ログインフォーム"""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control'
+            field.widget.attrs['placeholder'] = field.label  # placeholderにフィールドのラベルを入れる
+
+
+class UserCreateForm(UserCreationForm):
+    """ユーザー登録用フォーム"""
+
+    class Meta:
+        model = User
+        fields = ('email',)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control'
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        User.objects.filter(email=email, is_active=False).delete()
+        return email
+
+class StudentCreateForm(UserCreationForm):
+    """ユーザー登録用フォーム"""
+
+    class Meta:
+        model = Student
+        fields = ('email',)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control'
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        User.objects.filter(email=email, is_active=False).delete()
+        return email
+
+
+class SocietyCreateForm(UserCreationForm):
+    """サークル登録用フォーム"""
+
+    class Meta:
+        model = Society
+        fields = ('email',)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control'
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        Society.objects.filter(email=email, is_active=False).delete()
+        return email
