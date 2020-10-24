a6562aeef6a4596e3f11d028f13d4e3b83bcb3fd
Kkzs30842107 <kkzs.0125.30842107@gmail.com>
2020-10-24 15:08:22 +0900

    検索までやりました

---
+++ kikkawa/検索まで/demo/app/admin.py
@@ -0,0 +1,120 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from django.utils.translation import ugettext_lazy as _
+from .models import User, Student, Society
+
+
+class MyUserChangeForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+
+class MyUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('email',)
+
+
+class MyUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        #(_('Personal info'), {'fields': ('first_name', 'last_name','grade','school_name')}),
+        (_('Personal info'), {'fields': ('school_name', )}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'is_student', 'is_society',
+                                       'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+    form = MyUserChangeForm
+    add_form = MyUserCreationForm
+    #list_display = ('email', 'first_name', 'last_name', 'is_staff')
+    list_display = ('email', 'is_staff')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
+    #search_fields = ('email', 'first_name', 'last_name')
+    search_fields = ('email',)
+    ordering = ('email',)
+
+
+
+class MyStudentChangeForm(UserChangeForm):
+    class Meta:
+        model = Student
+        fields = '__all__'
+
+
+class MyStudentCreationForm(UserCreationForm):
+    class Meta:
+        model = Student
+        fields = ('email',)
+
+
+class MyStudentAdmin(UserAdmin):
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name','grade','school_name')}),
+        #(_('Personal info'), {'fields': ('school_name', )}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'is_student', 'is_society')}),
+                                       #'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+    form = MyStudentChangeForm
+    add_form = MyStudentCreationForm
+    #list_display = ('email', 'first_name', 'last_name', 'is_staff')
+    list_display = ('email', 'is_staff')
+    #list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
+    #search_fields = ('email', 'first_name', 'last_name')
+    search_fields = ('email',)
+    ordering = ('email',)
+
+
+
+class MySocietyChangeForm(UserChangeForm):
+    class Meta:
+        model = Society
+        fields = '__all__'
+
+
+class MySocietyCreationForm(UserCreationForm):
+    class Meta:
+        model = Society
+        fields = ('email',)
+
+
+class MySocietyAdmin(UserAdmin):
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('society_name','school_name')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+    form = MySocietyChangeForm
+    add_form = MySocietyCreationForm
+    list_display = ('email', 'society_name', 'is_staff')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
+    search_fields = ('email', 'society_name')
+    ordering = ('email',)
+
+
+admin.site.register(User, MyUserAdmin)
+admin.site.register(Student, MyStudentAdmin)
+admin.site.register(Society, MySocietyAdmin)
