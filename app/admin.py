fc596997f12112cfd483cadf05299ee5eac8af34
yut7G8 <billion7g8.316@keio.jp>
2020-10-28 19:49:37 +0900

    Tagのコメントアウトその2

--- app/admin.py
+++ app/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 from django.utils.translation import ugettext_lazy as _
-from .models import User, Student, Company, BoardModel, Tag
+from .models import User, Student, Company, BoardModel
+# from .model import Tag
 
 
 class MyUserChangeForm(UserChangeForm):
