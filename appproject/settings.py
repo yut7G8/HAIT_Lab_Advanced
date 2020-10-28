53e833b2998b916e9870a152152d27e4583cb87f
yut7G8 <billion7g8.316@keio.jp>
2020-10-28 18:32:57 +0900

    日本語/日本時間に変更

--- appproject/settings.py
+++ appproject/settings.py
@@ -105,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja' # 日本語に変更
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo' # 日本時間に変更
 
 USE_I18N = True
 
