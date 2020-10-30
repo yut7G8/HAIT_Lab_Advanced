1deaa5b3514fa28acb2c640c05638cdaee7e674d
yut7G8 <billion7g8.316@keio.jp>
2020-10-30 22:34:47 +0900

    見やすさ優先、urlpatternsの書き換え

--- appproject/urls.py
+++ appproject/urls.py
@@ -6,4 +6,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # 画像の読み込み用
