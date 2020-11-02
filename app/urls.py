acc025bb363c18121eeaae536dff4ca4d9e3c2a9
yut7G8 <billion7g8.316@keio.jp>
2020-11-02 20:28:29 +0900

    everypostforStudentのpathは一旦コメントアウト

--- app/urls.py
+++ app/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     # nagaya_develop_branchの変更箇所
     path('detail/', views.detailfunc, name='detailfun'), # views.pyのdetailfuncを参照
     path('detail/<int:post_id>/', views.everypost, name='everypost'), # views.pyのeverypost関数を参照
+    # path('detail/<int:post_id>/', views.everypostforStuednt, name='everypostforStudent'), # 学生側の閲覧用everypage
     path('add/', views.add, name='add'), # 投稿フォーム用のpath(仮)設定
     path('edit/<int:post_id>/', views.edit, name='edit'), # 編集機能の追加
     path('delete/<int:post_id>/', views.delete, name='delete'), # 削除機能の追加
